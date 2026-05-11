b09dcad4b400c28c0c08758a7d0d23e4617df980
RAZermenoSaracho <ricardozs_96@hotmail.com>
2026-05-11 15:15:26 -0600

    Checkpoint 4

---
+++ 04_test/src/controllers/tareas.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { obtenerTodasLasTareas, crearTarea } from "../services/tareas.service";
+
+export function getTareas(req: Request, res: Response): void {
+  res.json(obtenerTodasLasTareas());
+}
+
+export function postTarea(req: Request, res: Response): void {
+  const { titulo, descripcion } = req.body;
+
+  if (!titulo || !descripcion) {
+    res.status(400).json({ mensaje: "Faltan campos obligatorios" });
+    return;
+  }
+
+  const nueva = crearTarea({
+    titulo,
+    descripcion,
+    completada: false,
+  });
+
+  res.status(201).json(nueva);
+}
